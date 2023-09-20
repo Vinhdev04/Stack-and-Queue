889ac05cda848051a25c47e1f75ece38dd90dfd6
Phạm Công Vinh <124884232+Vinhdev04@users.noreply.github.com>
2023-09-20 14:42:03 +0700

    Update Postfix.cpp

--- Postfix.cpp
+++ Postfix.cpp
@@ -5,7 +5,7 @@
 
 #define MAX_SIZE 100
 
-// Ki?m tra xem m?t k? t? cÛ ph?i l‡ to·n t? hay khÙng
+// Kiem tra xem mot ky tu co phai la toan tu hay khong
 int isOperator(char ch) {
     if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
         return 1;
@@ -13,7 +13,7 @@ int isOperator(char ch) {
     return 0;
 }
 
-// Th?c hi?n phÈp tÌnh gi?a hai s? v?i m?t to·n t?
+// Thuc hien phep tinh giua hai so voi mot toan tu
 int performOperation(int operand1, int operand2, char operator) {
     if (operator == '+') {
         return operand1 + operand2;
@@ -27,28 +27,28 @@ int performOperation(int operand1, int operand2, char operator) {
     return 0;
 }
 
-// TÌnh gi· tr? c?a bi?u th?c ti?n t?
+// Tinh gia tri cua bieu thuc tien to
 int evaluatePrefix(char* prefix) {
     int i;
     int len = strlen(prefix);
     int stack[MAX_SIZE];
     int top = -1;
 
-    // Duy?t t? ph?i sang tr·i
+    // Duyet tu phai sang tratrai
     for (i = len - 1; i >= 0; i--) {
         if (prefix[i] == ' ') {
             continue;
-        } else if (isdigit(prefix[i])) { // K? t? l‡ s?
+        } else if (isdigit(prefix[i])) { // K? t? l√† s?
             int operand = 0;
             int j = i;
             while (j >= 0 && isdigit(prefix[j])) {
                 operand = (operand * 10) + (prefix[j] - '0');
                 j--;
             }
-            i = j + 1; // C?p nh?t l?i v? trÌ i
+            i = j + 1; // C?p nh?t l?i v? tr√≠ i
 
             stack[++top] = operand;
-        } else if (isOperator(prefix[i])) { // K? t? l‡ to·n t?
+        } else if (isOperator(prefix[i])) { // K? t? l√† to√°n t?
             int operand1 = stack[top--];
             int operand2 = stack[top--];
             int result = performOperation(operand1, operand2, prefix[i]);
@@ -59,13 +59,13 @@ int evaluatePrefix(char* prefix) {
     return stack[top];
 }
 
-// H‡m main
+// Ham main
 int main() {
     char prefix[MAX_SIZE];
 
     printf("Nhap vao bieu thuc tien to: ");
     fgets(prefix, MAX_SIZE, stdin);
-    prefix[strlen(prefix) - 1] = '\0'; // XÛa k? t? newline
+    prefix[strlen(prefix) - 1] = '\0'; // X√≥a k? t? newline
 
     int result = evaluatePrefix(prefix);
 
